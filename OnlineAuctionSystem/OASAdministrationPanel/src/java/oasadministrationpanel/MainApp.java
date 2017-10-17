9c2ee3ffdb602baeae81cf466752e15f954e8d16
lowru <lowru@172.25.112.175>
2017-10-18 00:56:48 +0800

    MainApp Menu done (without filling other methods)

--- OnlineAuctionSystem/OASAdministrationPanel/src/java/oasadministrationpanel/MainApp.java
+++ OnlineAuctionSystem/OASAdministrationPanel/src/java/oasadministrationpanel/MainApp.java
@@ -174,11 +174,78 @@ public class MainApp {
     }
 
     private void financeStaffMenu() {
+        Scanner scanner = new Scanner(System.in);
+        Integer response = 0;
 
+        while (true) {
+            System.out.println("*** OAS Finance Staff Menu ***\n");
+            System.out.println("1: Create Credit Package");
+            System.out.println("2: View Credit Package Details");
+            System.out.println("3: View All Credit Packages");
+            System.out.println("4: Back\n");
+            response = 0;
+
+            while (response < 1 || response > 4) {
+                System.out.print("> ");
+
+                response = scanner.nextInt();
+
+                if (response == 1) {
+                    createNewCreditPackage();
+                } else if (response == 2) {
+                    viewCreditPackageDetails();
+                } else if (response == 3) {
+                    viewAllCreditPackages();
+                } else if (response == 4) {
+                    break;
+                } else {
+                    System.out.println("Invalid option, please try again!\n");
+                }
+            }
+
+            if (response == 4) {
+                break;
+            }
+        }
     }
 
     private void salesStaffMenu() {
+        Scanner scanner = new Scanner(System.in);
+        Integer response = 0;
 
+        while (true) {
+            System.out.println("*** OAS Sales Staff Menu ***\n");
+            System.out.println("1: Create Auction Listing");
+            System.out.println("2: View Auction Listing Details");
+            System.out.println("3: View All Auction Listings");
+            System.out.println("4: View All Auction Listings with Bids Below Reserve Price");
+            System.out.println("5: Back\n");
+            response = 0;
+
+            while (response < 1 || response > 5) {
+                System.out.print("> ");
+
+                response = scanner.nextInt();
+
+                if (response == 1) {
+                    createAuctionListing();
+                } else if (response == 2) {
+                    viewAuctionListingDetails();
+                } else if (response == 3) {
+                    viewAllAuctionListings();
+                } else if (response == 4) {
+                    viewListingsBelowReservePrice();
+                } else if (response == 5) {
+                    break;
+                } else {
+                    System.out.println("Invalid option, please try again!\n");
+                }
+            }
+
+            if (response == 5) {
+                break;
+            }
+        }
     }
 
     private void changePassword() {
@@ -205,5 +272,33 @@ public class MainApp {
 
     private void viewAllEmployees() {
 
+    }
+    
+    private void createNewCreditPackage(){
+        
+    }
+    
+    private void viewCreditPackageDetails(){
+        
+    }
+    
+    private void viewAllCreditPackages(){
+        
+    }
+    
+    private void createAuctionListing(){
+        
+    }
+    
+    private void viewAuctionListingDetails(){
+        
+    }
+    
+    private void viewAllAuctionListings(){
+        
+    }
+    
+    private void viewListingsBelowReservePrice(){
+        
     }
 }
