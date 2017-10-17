30f0fbda639dfd46e1deb40a5ca3779335819a5f
lowru <lowru@172.25.112.175>
2017-10-18 02:10:10 +0800

    InvalidAccessRightException

---
+++ OnlineAuctionSystem/OnlineAuctionSystemLibrary/src/util/exception/InvalidAccessRightException.java
@@ -0,0 +1,11 @@
+package util.exception;
+
+public class InvalidAccessRightException extends Exception {
+
+    public InvalidAccessRightException() {
+    }
+
+    public InvalidAccessRightException(String msg) {
+        super(msg);
+    }
+}
