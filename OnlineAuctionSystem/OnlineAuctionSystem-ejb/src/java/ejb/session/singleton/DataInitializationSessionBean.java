48d86f6f4c196ad0912da67913448ed72038a182
Low Rui Jia <lowruijia96@gmail.com>
2017-11-04 10:46:21 +0800

    Initialise one auction listing to help test the subsequent use cases

--- OnlineAuctionSystem/OnlineAuctionSystem-ejb/src/java/ejb/session/singleton/DataInitializationSessionBean.java
+++ OnlineAuctionSystem/OnlineAuctionSystem-ejb/src/java/ejb/session/singleton/DataInitializationSessionBean.java
@@ -6,16 +6,19 @@
 package ejb.session.singleton;
 
 import ejb.session.stateless.AddressEntityControllerLocal;
+import ejb.session.stateless.AuctionListingEntityControllerLocal;
 import ejb.session.stateless.CreditPackageEntityControllerLocal;
 import ejb.session.stateless.CreditTransactionEntityControllerLocal;
 import ejb.session.stateless.CustomerEntityControllerLocal;
 import ejb.session.stateless.EmployeeEntityControllerLocal;
 import entity.AddressEntity;
+import entity.AuctionListingEntity;
 import entity.CreditPackageEntity;
 import entity.CreditTransactionEntity;
 import entity.CustomerEntity;
 import entity.EmployeeEntity;
 import java.math.BigDecimal;
+import java.util.Calendar;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.ejb.Singleton;
@@ -37,8 +40,11 @@ import util.exception.EmployeeNotFoundException;
 public class DataInitializationSessionBean {
 
     @EJB
+    private AuctionListingEntityControllerLocal auctionListingEntityControllerLocal;
+
+    @EJB
     private CreditPackageEntityControllerLocal creditPackageEntityControllerLocal;
-    
+
     @EJB
     private CreditTransactionEntityControllerLocal creditTransactionEntityControllerLocal;
 
@@ -47,69 +53,70 @@ public class DataInitializationSessionBean {
 
     @EJB
     private CustomerEntityControllerLocal customerEntityControllerLocal;
-    
+
     @EJB
     private EmployeeEntityControllerLocal employeeEntityControllerLocal;
-    
-    
-    
-    
 
     @PersistenceContext(unitName = "OnlineAuctionSystem-ejbPU")
     private EntityManager em;
 
     public DataInitializationSessionBean() {
     }
-    
+
     @PostConstruct
-    public void postConstruct()
-    {
-        try
-        {
+    public void postConstruct() {
+        try {
             employeeEntityControllerLocal.retrieveEmployeeByUsername("admin");
-        }
-        catch(EmployeeNotFoundException ex)
-        {
+        } catch (EmployeeNotFoundException ex) {
             initializeData();
         }
     }
-    
-    private void initializeData(){
+
+    private void initializeData() {
         employeeEntityControllerLocal.createEmployee(new EmployeeEntity("A", "A", EmployeeAccessRightEnum.SYSADMIN, "admin", "password"));
         EmployeeEntity financeEmployee = employeeEntityControllerLocal.createEmployee(new EmployeeEntity("B", "B", EmployeeAccessRightEnum.FINANCE, "finance", "password"));
         employeeEntityControllerLocal.createEmployee(new EmployeeEntity("C", "C", EmployeeAccessRightEnum.SALES, "sales", "password"));
-        
+
         CustomerEntity customerEntity = customerEntityControllerLocal.createCustomer(new CustomerEntity("A", "A", "123", "123", BigDecimal.ZERO, BigDecimal.ZERO, Boolean.FALSE, "customer", "password"));
-        
+
         AddressEntity addressEntity1 = addressEntityControllerLocal.createAddress(new AddressEntity("Heng Mui Keng", "10-10", "111111", Boolean.TRUE));
         AddressEntity addressEntity2 = addressEntityControllerLocal.createAddress(new AddressEntity("Heng Mui Keng", "10-11", "222222", Boolean.TRUE));
-        
+
         customerEntity.getAddressEntities().add(addressEntity1);
         customerEntity.getAddressEntities().add(addressEntity2);
-        
+
         CreditPackageEntity creditPackageEntity = new CreditPackageEntity("Basic Package", BigDecimal.valueOf(100), BigDecimal.valueOf(100), Boolean.TRUE);
         creditPackageEntity.setEmployeeEntity(financeEmployee);
         CreditPackageEntity creditPackageEntity2 = new CreditPackageEntity("Premium Package", BigDecimal.valueOf(300), BigDecimal.valueOf(320), Boolean.TRUE);
         creditPackageEntity2.setEmployeeEntity(financeEmployee);
         creditPackageEntity = creditPackageEntityControllerLocal.createNewCreditPackage(creditPackageEntity);
         creditPackageEntity2 = creditPackageEntityControllerLocal.createNewCreditPackage(creditPackageEntity2);
-        
+
         CreditTransactionEntity creditTransactionEntity1 = new CreditTransactionEntity(BigDecimal.valueOf(100), CreditTransactionTypeEnum.TOPUP);
         creditTransactionEntity1.setCreditPackageEntity(creditPackageEntity);
         creditTransactionEntity1.setCustomerEntity(customerEntity);
         creditTransactionEntityControllerLocal.createCreditTransactionEntity(creditTransactionEntity1);
-        
+
         customerEntity.setCreditBalance(customerEntity.getCreditBalance().add(creditTransactionEntity1.getNumberOfCredits()));
         //customerEntityControllerLocal.updateCustomer(customerEntity);
-        
+
         CreditTransactionEntity creditTransactionEntity2 = new CreditTransactionEntity(BigDecimal.valueOf(320), CreditTransactionTypeEnum.TOPUP);
         creditTransactionEntity2.setCreditPackageEntity(creditPackageEntity2);
         creditTransactionEntity2.setCustomerEntity(customerEntity);
         creditTransactionEntityControllerLocal.createCreditTransactionEntity(creditTransactionEntity2);
-        
+
         customerEntity.setCreditBalance(customerEntity.getCreditBalance().add(creditTransactionEntity2.getNumberOfCredits()));
         //customerEntityControllerLocal.updateCustomer(customerEntity);
-        
+
         creditPackageEntity2.getCreditTransactionEntities().add(creditTransactionEntity2);
+        
+        Calendar startDT = Calendar.getInstance();
+        startDT.set(2017, 10, 20, 00, 00);
+        
+        Calendar endDT = Calendar.getInstance();
+        endDT.set(2018, 01, 01, 00, 00);
+        
+        AuctionListingEntity auctionListingEntity = new AuctionListingEntity("itemA", BigDecimal.valueOf(20), startDT , endDT, BigDecimal.ZERO, Boolean.TRUE, Boolean.TRUE, addressEntity2);
+        auctionListingEntityControllerLocal.createAuctionListing(auctionListingEntity);
     }
 }
