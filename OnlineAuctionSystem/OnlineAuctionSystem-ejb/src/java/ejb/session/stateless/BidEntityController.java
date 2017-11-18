7c9a228c911b919f984affda22f29445f0388484
Soong Yi Xuan <yxsoong@gmail.com>
2017-11-19 00:41:37 +0800

    Sniping updated

--- OnlineAuctionSystem/OnlineAuctionSystem-ejb/src/java/ejb/session/stateless/BidEntityController.java
+++ OnlineAuctionSystem/OnlineAuctionSystem-ejb/src/java/ejb/session/stateless/BidEntityController.java
@@ -54,7 +54,8 @@ public class BidEntityController implements BidEntityControllerRemote, BidEntity
     public BidEntity createNewBid(BidEntity bidEntity) throws InvalidBidException {
         CustomerEntity customerEntity = em.find(CustomerEntity.class, bidEntity.getCustomerEntity().getCustomerId());
         AuctionListingEntity auctionListingEntity = em.find(AuctionListingEntity.class, bidEntity.getAuctionListingEntity().getAuctionListingId());
-
+        
+        System.out.println("checking " + bidEntity.getBidAmount() + " " + auctionListingEntity.getStartingBidAmount());
         try {
             if ((auctionListingEntity.getCurrentBidAmount() == null && bidEntity.getBidAmount().compareTo(auctionListingEntity.getStartingBidAmount()) > 0)
                     || bidEntity.getBidAmount().compareTo(auctionListingEntity.getCurrentBidAmount()) > 0) {
